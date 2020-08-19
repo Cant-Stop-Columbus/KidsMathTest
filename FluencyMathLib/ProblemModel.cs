dc46e7995acbf42e97a1ca04dd46ea6db4016237
Ria Jotsinghani <rjotsinghani@gmail.com>
2020-08-19 16:57:34 -0400

    problem model

---
+++ FluencyMathLib/ProblemModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FluencyMathLib
+{
+    public class ProblemModel
+    {
+        public List<int> Values { get; set; }
+        public string Method { get; set; }
+
+        public int Solution { get; set; }
+        public bool Result { get; set; }
+
+        public int Remainder { get; set; }
+
+
+
+
+       
+    }
+}
